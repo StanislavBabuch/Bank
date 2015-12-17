70c4d18b664f198da13bf61f4bfc8b18067773ad
Стас Бабич < >
2015-12-17 10:17:25 +0200

    Add project Money

---
+++ Money/Money.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Money
+{
+    public class Money
+    {
+        public int Amount { get; set; }
+    }
+}
